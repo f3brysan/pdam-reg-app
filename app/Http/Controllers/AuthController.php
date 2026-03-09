dba52f97e876f5745123572a61befda8058f1bb9
Febry San <97709170+f3brysan@users.noreply.github.com>
2026-03-09 21:45:44 +0700

    Add registration functionality with validation in AuthController and create registration view

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -37,6 +40,30 @@ class AuthController extends Controller
     public function register()
     {
         return view('auth.register');
+    }
+
+    public function registerStore(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
+        if ($validate->fails()) {
+            return redirect()->route('register')->with('error', $validate->errors()->first())->withInput();
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $user->assignRole('user');
+
+        return redirect()->route('login')->with('success', 'Registration successful. Please login to continue');
+        
     }
     
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,3 +8,8 @@ Route::get('/', function () {
 });
 
 
+route::get('/login', [AuthController::class, 'login'])->name('login');
+route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
+route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+route::get('/register', [AuthController::class, 'register'])->name('register');
+route::post('/register', [AuthController::class, 'registerStore'])->name('register.store');
