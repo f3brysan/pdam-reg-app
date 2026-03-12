f085c8e513f6dd00e2ed18892449c40082785582
Febry San <97709170+f3brysan@users.noreply.github.com>
2026-03-12 23:58:06 +0700

    Refactor routes for master resources by grouping under a 'master' prefix for better organization and readability. This includes routes for Pekerjaan, Jenis Meteran, and Jenis Tempat Tinggal with role-based access for admin users.

--- routes/web.php
+++ routes/web.php
@@ -22,20 +22,25 @@ route::group(['middleware' => 'auth'], function () {
     route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     route::group(['middleware' => ['role:admin']], function () {
-        route::get('/master/pekerjaan', [MsPekerjaanController::class, 'index'])->name('ms_pekerjaans.index');
-        route::post('/master/pekerjaan', [MsPekerjaanController::class, 'store'])->name('ms_pekerjaans.store');
-        route::get('/master/pekerjaan/{id}', [MsPekerjaanController::class, 'show'])->name('ms_pekerjaans.show');
-        route::post('/master/pekerjaan/delete', [MsPekerjaanController::class, 'destroy'])->name('ms_pekerjaans.delete');
+        // * Master (prefix)
+        Route::prefix('master')->group(function () {
+            // * Master Pekerjaan
+            route::get('/pekerjaan', [MsPekerjaanController::class, 'index'])->name('ms_pekerjaans.index');
+            route::post('/pekerjaan', [MsPekerjaanController::class, 'store'])->name('ms_pekerjaans.store');
+            route::get('/pekerjaan/{id}', [MsPekerjaanController::class, 'show'])->name('ms_pekerjaans.show');
+            route::post('/pekerjaan/delete', [MsPekerjaanController::class, 'destroy'])->name('ms_pekerjaans.delete');
 
-        route::get('/master/jenis-meteran', [MsJenisMeteranController::class, 'index'])->name('ms_jenis_meterans.index');
-        route::post('/master/jenis-meteran', [MsJenisMeteranController::class, 'store'])->name('ms_jenis_meterans.store');
-        route::get('/master/jenis-meteran/{id}', [MsJenisMeteranController::class, 'show'])->name('ms_jenis_meterans.show');
-        route::post('/master/jenis-meteran/delete', [MsJenisMeteranController::class, 'destroy'])->name('ms_jenis_meterans.delete');
+            // * Master Jenis Meteran
+            route::get('/jenis-meteran', [MsJenisMeteranController::class, 'index'])->name('ms_jenis_meterans.index');
+            route::post('/jenis-meteran', [MsJenisMeteranController::class, 'store'])->name('ms_jenis_meterans.store');
+            route::get('/jenis-meteran/{id}', [MsJenisMeteranController::class, 'show'])->name('ms_jenis_meterans.show');
+            route::post('/jenis-meteran/delete', [MsJenisMeteranController::class, 'destroy'])->name('ms_jenis_meterans.delete');
 
-        
-        route::get('/master/jenis-tempat-tinggal', [MsJenisTempatTinggalController::class, 'index'])->name('ms_jenis_tempat_tinggals.index');
-        route::post('/master/jenis-tempat-tinggal', [MsJenisTempatTinggalController::class, 'store'])->name('ms_jenis_tempat_tinggals.store');
-        route::get('/master/jenis-tempat-tinggal/{id}', [MsJenisTempatTinggalController::class, 'show'])->name('ms_jenis_tempat_tinggals.show');
-        route::post('/master/jenis-tempat-tinggal/delete', [MsJenisTempatTinggalController::class, 'destroy'])->name('ms_jenis_tempat_tinggals.delete');
-    });    
+            // * Master Jenis Tempat Tinggal
+            route::get('/jenis-tempat-tinggal', [MsJenisTempatTinggalController::class, 'index'])->name('ms_jenis_tempat_tinggals.index');
+            route::post('/jenis-tempat-tinggal', [MsJenisTempatTinggalController::class, 'store'])->name('ms_jenis_tempat_tinggals.store');
+            route::get('/jenis-tempat-tinggal/{id}', [MsJenisTempatTinggalController::class, 'show'])->name('ms_jenis_tempat_tinggals.show');
+            route::post('/jenis-tempat-tinggal/delete', [MsJenisTempatTinggalController::class, 'destroy'])->name('ms_jenis_tempat_tinggals.delete');
+        });
+    });
 });
